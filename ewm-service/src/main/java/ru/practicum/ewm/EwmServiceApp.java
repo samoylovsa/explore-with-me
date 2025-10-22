a16fe30cafc4bf348841e7ea860687ca71fe594c
Sergey Samoylov <s.samoylov@cardsmobile.ru>
2025-10-22 22:24:00 +0300

    Fix docker-compose.yml and main service name

--- ewm-service/src/main/java/ru/practicum/ewm/EwmServiceApp.java
+++ ewm-service/src/main/java/ru/practicum/ewm/EwmServiceApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainServiceApp {
+public class EwmServiceApp {
     public static void main(String[] args) {
-        SpringApplication.run(MainServiceApp.class, args);
+        SpringApplication.run(EwmServiceApp.class, args);
     }
 }
