b7c99dcca44bf8b4c75f780bca60e774a74568a5
Sergey Samoylov <s.samoylov@cardsmobile.ru>
2025-10-23 22:59:32 +0300

    Implemented GET /stats method

--- stats/stats-server/src/main/java/ewm/controller/StatsController.java
+++ stats/stats-server/src/main/java/ewm/controller/StatsController.java
@@ -1,16 +1,20 @@
 package ewm.controller;
 
-import dto.EndpointHitDto;
+import dto.GetStatsDto;
+import dto.SaveHitDto;
+import dto.StatsRequest;
 import ewm.service.StatsService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Slf4j
 @Validated
@@ -22,8 +26,26 @@ public class StatsController {
 
     @PostMapping("/hit")
     @ResponseStatus(HttpStatus.CREATED)
-    public void saveStats(@Valid @RequestBody EndpointHitDto requestBody) {
+    public void saveStats(@Valid @RequestBody SaveHitDto requestBody) {
         log.info("Received save stats request: {}", requestBody);
         statsService.saveStats(requestBody);
+    }
+
+    @GetMapping("/stats")
+    public List<GetStatsDto> getStats(
+            @RequestParam @NotNull @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+            @RequestParam @NotNull @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+            @RequestParam(required = false) List<String> uris,
+            @RequestParam(required = false, defaultValue = "false") Boolean unique) {
+        log.info("Received stats request: start={}, end={}, uris={}, unique={}", start, end, uris, unique);
+
+        StatsRequest statsRequest = StatsRequest.builder()
+                .start(start)
+                .end(end)
+                .uris(uris)
+                .unique(unique)
+                .build();
+
+        return statsService.getStats(statsRequest);
     }
 }
