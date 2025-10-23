19c12f16690a45bcde0f153e098023067dbf7804
Alexander Markov <pwney@yandex.ru>
2025-10-23 19:07:54 +0300

    Добавлены DTO сущностей сервиса статистики

---
+++ stats/stats-dto/src/main/java/dto/StatsRequest.java
@@ -0,0 +1,27 @@
+package dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatsRequest {
+    @NotNull
+    @PastOrPresent
+    LocalDateTime start;
+
+    @NotNull
+    @PastOrPresent
+    LocalDateTime end;
+
+    List<String> uris;
+
+    Boolean unique;
+}
