319cea8607957c28ab981e179caa57e9098701a2
Sergey Samoylov <sam0lov@yandex.ru>
2025-10-28 21:42:53 +0300

    Implemented error handler

---
+++ ewm-service/src/main/java/ewm/exception/ErrorHandler.java
@@ -0,0 +1,26 @@
+package ewm.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleException(Exception ex, HttpStatus httpStatus) {
+        log.warn("Error 500: {}", ex.getMessage(), ex);
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        ex.printStackTrace(printWriter);
+        String stackTrace = stringWriter.toString();
+        return new ErrorResponse(httpStatus, ex.getMessage(), stackTrace);
+    }
+}
