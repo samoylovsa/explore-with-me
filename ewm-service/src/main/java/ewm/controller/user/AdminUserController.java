8e080047730cbfda57e490de3c6fd18b899d1ad3
Alexander Markov <pwney@yandex.ru>
2025-10-31 19:38:05 +0300

    Fix admin users endpoint

--- ewm-service/src/main/java/ewm/controller/user/AdminUserController.java
+++ ewm-service/src/main/java/ewm/controller/user/AdminUserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("/admin/users")
 @Validated
 public class AdminUserController {
     private final UserService userService;
