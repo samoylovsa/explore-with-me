54eaf4804c80a488827d26d45b0c0977bac716e6
a11nxght <a11nxght@yandex.kz>
2025-10-31 20:41:11 +0300

    реализовать сущность Event и приватный API (создание, просмотр, обновление, отмена)

---
+++ ewm-service/src/main/java/ewm/dto/event/EventShortDto.java
@@ -0,0 +1,20 @@
+package ewm.dto.event;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class EventShortDto {
+    private Long id;
+    private String title;
+    private String annotation;
+    private CategoryDto category; // id, name
+    private Boolean paid;
+    private LocalDateTime eventDate;
+    private UserShortDto initiator; // id, name
+    private Integer confirmedRequests; // пока 0
+    private Long views; // пока 0
+}
