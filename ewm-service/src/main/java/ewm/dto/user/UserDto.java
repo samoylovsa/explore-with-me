43c8894a12fe97a3e68ca9e7b1d74a9e34c29968
a11nxght <a11nxght@yandex.kz>
2025-11-02 22:56:19 +0300

    After rebase

--- ewm-service/src/main/java/ewm/dto/user/UserDto.java
+++ ewm-service/src/main/java/ewm/dto/user/UserDto.java
@@ -2,7 +2,7 @@ package ewm.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -14,12 +14,12 @@ import lombok.*;
 public class UserDto {
     private Long id;
 
-    @NotNull(message = "Имя пользователя должно быть в теле запроса")
     @NotBlank(message = "Имя пользователя не может быть пустым")
+    @Size(min = 2, max = 250)
     private String name;
 
-    @NotNull(message = "Адрес электронной почты должен быть в теле запроса")
     @NotBlank(message = "Адрес электронной почты не может быть пустым")
     @Email(message = "Некорректный адрес электронной почты")
+    @Size(min = 6, max = 254)
     private String email;
 }
