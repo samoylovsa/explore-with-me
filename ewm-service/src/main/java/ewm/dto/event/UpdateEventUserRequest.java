cd351ec2521333b73a61d497972ea950f7564693
a11nxght <a11nxght@yandex.kz>
2025-11-02 22:56:19 +0300

    тесты

--- ewm-service/src/main/java/ewm/dto/event/UpdateEventUserRequest.java
+++ ewm-service/src/main/java/ewm/dto/event/UpdateEventUserRequest.java
@@ -1,5 +1,8 @@
 package ewm.dto.event;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,12 +11,22 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 public class UpdateEventUserRequest {
+
+    @Size(min = 3, max = 120)
     private String title;
+
+    @Size(min = 20, max = 7000)
     private String description;
+
+    @Size(min = 20, max = 2000)
     private String annotation;
     private Long category;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime eventDate;
     private Boolean paid;
+
+    @PositiveOrZero
     private Integer participantLimit;
     private Boolean requestModeration;
     private StateAction stateAction;
