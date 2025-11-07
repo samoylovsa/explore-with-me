a1415707e0ccec4b51d00aff85129e2956e61b7a
Evgeniya <eva.sharapova1@mail.ru>
2025-11-07 13:09:40 +0300

    change for test stats server

--- stats/stats-server/src/main/java/stats/server/exception/ErrorHandler.java
+++ stats/stats-server/src/main/java/stats/server/exception/ErrorHandler.java
@@ -2,6 +2,7 @@ package stats.server.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -28,6 +29,17 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidationException(ValidationException ex) {
         log.warn("Validation error: {}", ex.getMessage(), ex);
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        ex.printStackTrace(printWriter);
+        String stackTrace = stringWriter.toString();
+        return new ErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), stackTrace);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMissingServletRequestParameter(MissingServletRequestParameterException ex) {
+        log.warn("Missing params error: {}", ex.getMessage(), ex);
         StringWriter stringWriter = new StringWriter();
         PrintWriter printWriter = new PrintWriter(stringWriter);
         ex.printStackTrace(printWriter);
